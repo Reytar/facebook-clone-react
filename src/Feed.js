9f186660237a84c8265d9b143b5d9fa8feb39124
reytar <tzur58@gmail.com>
2021-01-02 18:45:02 +0200

    db support + login

--- src/Feed.js
+++ src/Feed.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Feed.css";
 import MessageSender from "./MessageSender";
 import Post from "./Post";
 import StoryReel from "./StoryReel";
+import db from "./firebase";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+      );
+  }, []);
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
-      <Post
-        profilePic="https://avatars3.githubusercontent.com/u/64041038?s=400&u=75196d0f6df79c05732f67ae4e52a58d2add7ed5&v=4"
-        message="Woooow Wokrs"
-        timestamp="time"
-        username="username"
-        image="https://bi.im-g.pl/im/0b/a2/18/z25829387V,las--zdjecie-ilustracyjne---Od-3-kwietnia-obowiazu.jpg"
-      />
-      <Post
-        profilePic="https://avatars3.githubusercontent.com/u/64041038?s=400&u=75196d0f6df79c05732f67ae4e52a58d2add7ed5&v=4"
-        message="Woooow Wokrs"
-        timestamp="time"
-        username="username"
-        image=""
-      />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          profilePic={post.data.profilePic}
+          message={post.data.message}
+          timestamp={post.data.timestamp}
+          username={post.data.username}
+          image={post.data.image}
+        />
+      ))}
     </div>
   );
 }
